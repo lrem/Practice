9291120a34d0a591584ec0a97a39e87071f227c2
zj203448944 <203448944@qq.com>
2016-01-20 11:16:21 +0800

    添加多线程例子

---
+++ src/main/java/com/apache/example/multithreading/BallComponent.java
@@ -0,0 +1,24 @@
+package com.apache.example.multithreading;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created by zhongjing on 2016/1/20 0020.
+ */
+public class BallComponent extends JPanel {
+    public void add(Ball b) {
+        balls.add(b);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            g2.fill(b.getShape());
+        }
+    }
+
+    private ArrayList<Ball> balls = new ArrayList<Ball>();
+}
